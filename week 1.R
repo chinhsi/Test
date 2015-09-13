7ad5eff05333af6eece3d9d157969e317aabdb2a
chinhsi <chinhsi@unknown542696dc4c49.attlocal.net>
2015-09-12 22:28:18 -0400

    test

---
+++ week 1.R
@@ -0,0 +1,38 @@
+## Week 1####
+
+demo()
+
+help(lm)
+
+?lm()
+
+help.search("regression")
+
+##
+1+3
+1-3
+2*4
+3/5
+squt(8)
+2^4
+3^3
+2**4
+3**2
+10 %% 3
+10 %/% 3
+
+## logical operators
+a = 3:5
+b = 5:7
+
+a > 4
+a < 4
+a <=4
+a >=4
+a ==4
+a !=4
+
+## logical operators combined
+(a==4) & (b==6)
+(a==4) | (b>5)
+
